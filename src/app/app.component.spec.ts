7de6d9c4dbd020fa9d23fbd5335237af83e63ac0
Tanushree-jain <tanushreegangwal19@gmail.com>
2024-05-09 11:35:30 +0530

    update docs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('hello');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, youtube_sample');
   });
 });
